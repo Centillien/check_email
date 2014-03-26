92dc3fe3308f9e71102d7965d5d14d8eed334af7
Gerard Kanters <gkanters@gmail.com>
2014-03-26 01:20:22 +0100

    Create test.php
    Example file to test the existence of the email address.

---
+++ test.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Sample</title>
+    </head>
+<body>
+        <?php
+        require_once 'class.check_email.php';
+
+        $anon_email = 'not_existing_email123@gmail.com';
+
+        $vmail = new verifyEmail();
+
+        if ($vmail->check_email($anon_email) == false) {
+                echo "That email address does not seem to exist;
+        }else{
+                echo "That email does indeed exists";
+        }
+
+        ?>
+    </body>
+</html>
